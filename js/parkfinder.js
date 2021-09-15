8dd59ba89603a056410dddf279d2a6a8a86bd025
SethThomaspowered <athomas.seth@gmail.com>
2021-09-14 22:05:47 -0400

    added selectedZipCode variable

--- js/parkfinder.js
+++ js/parkfinder.js
@@ -3,6 +3,7 @@ const app = new Vue ({
 	data() {
 		return {
 			selectedPark: "",
+            selectedZipCode: 0,
             parks: [
 			{id: 1,	parkName: '35TH STREET', 	address: "814 SOUTH 35TH ST",	zipCode: 40211, visited: false,},
             {id: 2,	parkName: "CRESCENT HILL",	address: "3110 BROWNSBORO RD",	zipCode: 40206, visited: false,},
