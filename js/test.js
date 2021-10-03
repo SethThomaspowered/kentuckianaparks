15b840edaedf7d1227f21ea74c38659ef7e0134b
SethThomaspowered <athomas.seth@gmail.com>
2021-10-03 18:23:39 -0400

    search bar changes

--- js/test.js
+++ js/test.js
@@ -1,14 +1,34 @@
-const parkChecks = {
-    zipCodeCheck : document.getElementById('ZipCodeCheck'),
-    cityCheck : document.getElementById('CityCheck'),
-    countyCheck : document.getElementById('CountyCheck')
-}
-for (check in parkChecks) {check.addEventListener('click', function(){
-    const zip = document.getElementById('zipcode')
-    if (zip.classList.contains('d-none')) {
-        zip.classList.remove('d-none');
-    }
-    else {
-        zip.classList.add('d-none');
-    }
-})}
+//Trying to create search options by clicking on radio buttons.
+const zipCodeCheck = document.getElementById('ZipCodeCheck'),
+    cityCheck = document.getElementById('CityCheck'),
+    countyCheck = document.getElementById('CountyCheck'),
+    zip = document.getElementById('zipcode'),
+    county = document.getElementById('county'),
+    city = document.getElementById('city');
+
+
+zipCodeCheck.addEventListener('click', function(){
+    if (zipCodeCheck) {
+        if (zip.classList.contains('d-none')) {
+            zip.classList.remove('d-none');
+        }
+        else {
+            zip.classList.add('d-none');
+        }}})
+countyCheck.addEventListener('click', function(){    
+    if (countyCheck) {
+        if (county.classList.contains('d-none')) {
+            county.classList.remove('d-none');
+    }else {
+        county.classList.add('d-none');
+}}
+})
+cityCheck.addEventListener('click', function(){    
+    if (cityCheck) {
+        if (city.classList.contains('d-none')) {
+            city.classList.remove('d-none');
+        }
+        else {
+            city.classList.add('d-none');
+}}
+})
