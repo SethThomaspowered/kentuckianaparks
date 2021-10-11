91b307fda31b52afa7dc594b0e9fd276f763e7e6
SethThomaspowered <athomas.seth@gmail.com>
2021-10-11 16:03:26 -0400

    search features

--- js/parkfinder.js
+++ js/parkfinder.js
@@ -10,7 +10,6 @@ const app = new Vue ({
             randomId: Math.floor((Math.random() * 290) + 1),
             parks: parks,
             searchParam: '',
-<<<<<<< HEAD
             search: 'walking',
             featuresList: ['disc golf', 'golf', 'walking', 'basketball']
         }
@@ -23,16 +22,3 @@ const app = new Vue ({
         }
     } 
 })
-=======
-            zipCodeCheck : document.getElementById('submit'),
-            zip : document.getElementById('zipcode'),
-            county : document.getElementById('county'),
-            city : document.getElementById('city')
-        }
-	},
-     methods: {
-        
-         }
-     },
-)
->>>>>>> f2cfa2920c6623bf94fe0cf797c45eae1ddeec28
